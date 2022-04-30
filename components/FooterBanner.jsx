1b676044e347fba28ed8b5e2f27d17ea14d1b0b6
tyler <tymaggio03@gmail.com>
2022-04-30 15:28:17 -0600

    Refactored

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FooterBanner = ({ footerBanner: discount, largeText1, largeText2, saleTime }) => {
+const FooterBanner = ({ footerBanner: discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image }) => {
   return (
     <div className='footer-banner-container'>
       <div className='banner-desc'>
@@ -11,8 +11,14 @@ const FooterBanner = ({ footerBanner: discount, largeText1, largeText2, saleTime
           <p>{saleTime}</p>
         </div>
         <div className='right'>
-
+            <p>{smallText}</p>
+            <h3>{midText}</h3>
+            <p>{desc}</p>
+            <Link href={`/product/${product}`}>
+              <button type='button'>{buttonText}</button>
+            </Link>
         </div>
+        <img src={urlFor(image)} className='footer-banner-image' />
       </div>
     </div>
   )
