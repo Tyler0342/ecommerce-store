69138ea7286bc32e837adc5bd4e070df00ca3d8e
tyler <tymaggio03@gmail.com>
2022-04-30 15:24:25 -0600

    Backend framework complete, dynamic footer banner added

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const FooterBanner = () => {
+const FooterBanner = ({ footerBanner: discount, largeText1, largeText2, saleTime }) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className='right'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>Speakers</p>
+      <h2>Best Selling Products</h2>
+      <p>Headphones</p>
     </div>
 
     <div className="products-container">
