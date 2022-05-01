2247e8f06fadbd27cc102d3c503bfcac8d278bda
tyler <tymaggio03@gmail.com>
2022-05-01 11:49:12 -0600

    Footer and banners implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Layout } from '../components';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      // Component = The component user is currently on
+      <Component {...pageProps} />
+    </Layout> 
+  )
 }
 
 export default MyApp
